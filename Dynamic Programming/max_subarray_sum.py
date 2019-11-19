be3780417a8a019d401c9dcd73485e9069140f2e
Мето Трајковски <metot@hotmail.com>
2019-11-20 00:04:09 +0100

    Changed max_subarray_sum

--- Dynamic Programming/max_subarray_sum.py
+++ Dynamic Programming/max_subarray_sum.py
@@ -1,7 +1,7 @@
 '''
 Maximum subarray sum 
 
-The subarray must be contiguous
+The subarray must be contiguous.
 
 Sample input: [-2, -3, 4, -1, -2, 1, 5, -3]
 Sample output: 7
@@ -21,29 +21,21 @@ After each addition, check if the current sum is greater than the max sum. (Call
 ############
 
 def max_subarray_sum(a):
-    # max_sum, max_start_idx, max_end_idx
-    max_sum = (0, 0, 0)
-
     i = 0
     n = len(a)
     curr_sum = 0
-    start_idx = 0
+    max_sum = 0
 
     while i < n:
-        curr_sum += a[i]
+        # restart the current sum if 0 or smaller, we care only about the positive sums
+        curr_sum = max(0, curr_sum + a[i])
 
-        if curr_sum <= 0:
-            # restart the current sum, we care only about positive sums
-            start_idx = i + 1
-            curr_sum = 0
-        else:
-            # check if this is the max sum
-            if curr_sum > max_sum[0]:
-                max_sum = (curr_sum, start_idx, i + 1)    
+        # check if this is the max sum
+        max_sum = max(max_sum, curr_sum)
 
         i += 1
 
-    return max_sum[0]
+    return max_sum
 
 
 ###########
