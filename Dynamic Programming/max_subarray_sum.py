906b91d884ced806bb64c117e79c3c1486c7e601
pkacprzak <pawel.kacprzak.89@gmail.com>
2019-12-16 17:55:29 +0100

    more pythonic iteration; positive -> non-negative in the description; added test with all negative numbers

--- Dynamic Programming/max_subarray_sum.py
+++ Dynamic Programming/max_subarray_sum.py
@@ -9,7 +9,7 @@ Output explanation: [4, -1, -2, 1, 5]
 
 =========================================
 Need only one iteration, in each step add the current element to the current sum.
-When the sum is equal or less than 0, reset the sum to 0 and continue with adding. (we care only about the positive sums)
+When the sum is less than 0, reset the sum to 0 and continue with adding. (we care only about non-negative sums)
 After each addition, check if the current sum is greater than the max sum. (Called Kadane's algorithm)
     Time Complexity:    O(N)
     Space Complexity:   O(1)
@@ -21,19 +21,16 @@ After each addition, check if the current sum is greater than the max sum. (Call
 ############
 
 def max_subarray_sum(a):
-    i = 0
-    n = len(a)
     curr_sum = 0
     max_sum = 0
 
-    while i < n:
-        # restart the current sum if 0 or smaller, we care only about the positive sums
-        curr_sum = max(0, curr_sum + a[i])
+    for val in a:
+        # extend the current sum with the curren value; 
+        # reset it to 0 if it is smaller than 0, we care only about non-negative sums
+        curr_sum = max(0, curr_sum + val)
 
         # check if this is the max sum
         max_sum = max(max_sum, curr_sum)
-
-        i += 1
 
     return max_sum
 
@@ -53,3 +50,7 @@ print(max_subarray_sum([1, -2, 2, -2, 3, -2, 4, -5]))
 # Test 3
 # Correct result => 7
 print(max_subarray_sum([-2, -5, 6, -2, -3, 1, 5, -6]))
+
+# Test 4
+# Correct result => 0
+print(max_subarray_sum([-6, -1]))
