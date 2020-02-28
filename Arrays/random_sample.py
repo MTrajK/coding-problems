0c6ea28e3df8edcad7695fb8c3fdf75703048766
Meto Trajkovski <Meto.Trajkovski@3shape.com>
2020-02-28 14:22:02 +0100

    Added random sample algorithm.

---
+++ Arrays/random_sample.py
@@ -0,0 +1,75 @@
+'''
+Random Sample
+
+Given an array and length of the sample, find a random sample from that array.
+
+Input: [1, 2, 3, 4], 2
+Output: This is a nondeterministic algorithm, C(N, K) combinations exist.
+    In this case 4! / (2! * (4 - 2)!) = 6. All combinations are a valid solution.
+    [1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]
+
+=========================================
+Simple solution in one pass, Reservoir sampling. Maybe the solution looks like the elements don't have an equal probability
+to be chosen, but there is a proof that they have equal probability https://en.wikipedia.org/wiki/Reservoir_sampling
+    Time Complexity:    O(N)
+    Space Complexity:   O(K)
+Another simple solution in one pass, the probability for each element to be choosen in thee.
+    Time Complexity:    O(N)
+    Space Complexity:   O(K)
+'''
+
+
+##############
+# Solution 1 #
+##############
+
+from random import randint
+
+def reservoir_sampling(arr, k):
+    # fill the reservoir array
+    sample = []
+    for i in range(k):
+        sample.append(arr[i])
+
+    # replace elements with gradually decreasing probability
+    n = len(arr)
+    for i in range(k, n):
+        # randint(a, b) generates a uniform integer from the inclusive range {a, ..., b} (a <= X <= b)
+        j = randint(0, i)
+        if j < k:
+            sample[j] = arr[i]
+
+    return sample
+
+
+##############
+# Solution 2 #
+##############
+
+from random import random
+
+def probabilistic_sampling(arr, k):
+    sample = []
+    n = len(arr)
+
+    for el in arr:
+        # random() generates a uniform double in this range (0 <= X < 1)
+        # (k / n) is the probability for this element to be choosen (0 <= X <= 1)
+        if random() < (k / n):
+            sample.append(el)
+            k -= 1 # left elements to be choosen
+        n -= 1 # left elements for choosing
+
+    return sample
+
+
+###########
+# Testing #
+###########
+
+# Test 1
+# Correct result => One of these: [1, 2], [1, 3], [1, 4], [2, 3], [2, 4]
+arr = [1, 2, 3, 4]
+k = 2
+print(reservoir_sampling(arr, k))
+print(probabilistic_sampling(arr, k))
--- Arrays/shuffle_array.py
+++ Arrays/shuffle_array.py
@@ -7,7 +7,7 @@ This question is also asked as “shuffle a deck of cards” or “randomize a g
 Here shuffle means that every permutation of array element should equally likely.
 
 Input: [1, 2, 3]
-Output: This is a nondeterministic algorithm, N! solutions/permutations exists.
+Output: This is a nondeterministic algorithm, N! solutions/permutations exist.
     In this case 3! = 6. All permutations are a valid solution.
     [1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]
 
@@ -15,8 +15,9 @@ Output: This is a nondeterministic algorithm, N! solutions/permutations exists.
 Easy in-place solution, choose an random index/position from I (I is the current position) to N-1
 (or choose from 0 to I, it's same), swap the element at random position with the element at I position.
 Increase I and continue with the same algorithm.
+This algorithm is called Fisher–Yates shuffle (https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle).
 Note: In Python there is already implemented shuffle (in random module "from random import shuffle") method
-    which works in similar way. But it's easy to be implemented and I wanted to show how to implement it.
+which works in similar way. But it's easy to be implemented and I wanted to show how to implement it.
     Time Complexity:    O(N)
     Space Complexity:   O(1)
 '''
