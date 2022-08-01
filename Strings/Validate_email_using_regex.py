440d2c6a4dc68b637285c381cb2c63478414b6cb
Hamid Reza Bakhtaki <hamidreza.bakhtaki@gmail.com>
2022-08-01 14:59:41 +0430

    Update Validate_email_using_regex.py

--- Strings/Validate_email_using_regex.py
+++ Strings/Validate_email_using_regex.py
@@ -1,35 +1,24 @@
 '''
-Solution to the problem "Validating and Parsing Email Addresses
-from the HackerRank challenge.
-'''
-# -----------------------------------------------------------------------
-# Given  pairs of names and email addresses as input,
-# print each name and email address pair having a valid email
-# address on a new line.
+Given  pairs of names and email addresses as input,print each name and email address pair having a valid email
+address on a new line.
 
-# Input Format
-# The first line contains a single integer,N ,
-# denoting the number of email address.
-# Each line i of the n subsequent lines contains a name and an email address
-# as two space-separated values following this format:
-# name <user@email.com>
-#
-# Constraints:
-# 0 <= N <= 100
-# Print the space-separated name and email address pairs containing valid email
-# addresses only.
-# Each pair must be printed on a new line in the following format:
-# name <user@email.com>
-#
-# Sample Input:
-# 2
-# DEXTER <dexter@hotmail.com>
-# VIRUS <virus!@variable.:p>
-#
-# Sample Output:
-# DEXTER <dexter@hotmail.com>
-# -----------------------------------------------------------------------
-# Solution:
+===============================================================
+Input:
+2
+DEXTER <dexter@hotmail.com>
+VIRUS <virus!@variable.:p>
+
+Output:
+DEXTER <dexter@hotmail.com>
+
+=================================================================
+
+'''
+
+############
+# Solution #
+############
+
 # Import the necessary modules.
 import re
 import email.utils
