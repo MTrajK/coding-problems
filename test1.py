af0ef3f6eb1ad3cb091a2d58425b6b544c06a11b
RidaIftikhar14 <122225638+RidaIftikhar14@users.noreply.github.com>
2023-02-24 01:59:01 +0500

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,4 +1,8 @@
 from Arrays.find_peak_element import find_peak_element
 from Arrays.find_unpaired import find_unpaired_element
-assert find_peak_element([1,5,3,1])==1
-assert find_unpaired_element([1,5,3,4,1,5,4])==3
+
+def test_peak():
+  assert find_peak_element([1,5,3,1])==1
+  
+def test_unpaired():
+  assert find_unpaired_element([1,5,3,4,1,5,4])==3
