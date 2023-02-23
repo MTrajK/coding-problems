9c21b370da91f1f6821a7e0f4c1c269d9569c25e
faizaashraf28 <106381415+faizaashraf28@users.noreply.github.com>
2023-02-24 00:59:28 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
-import requests
+from coding-problems.Math.odd_sum import odd_sum
 
-url = "https://github.com/faizaashraf28/coding-problems/blob/master/Math/odd_sum.py"
-response = requests.get(url)
-filename = response.content
 
-exec(filename)
 # Test 1
 # Correct result => 24
 assert odd_sum(1,3)== 4
