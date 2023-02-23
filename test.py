04e9dca7f5472f1438fd9515c9dd8a0ad8c24ece
faizaashraf28 <106381415+faizaashraf28@users.noreply.github.com>
2023-02-24 00:48:26 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-import Math.odd_sum.py
+import requests
+
+url = ""
+response = requests.get(url)
+filename = response.content
+
+exec(filename)
 # Test 1
 # Correct result => 24
 assert odd_sum(1,3)== 4
