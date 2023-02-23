406d489120bdfbbfa36ee546ba27cb843bbda24a
faizaashraf28 <106381415+faizaashraf28@users.noreply.github.com>
2023-02-24 01:56:39 +0500

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,4 @@
+from Arrays.find_peak_element import find_peak_element
+from Arrays.find_unpaired import find_unpaired_element
+assert find_peak_element([1,5,3,1])==1
+assert find_unpaired_element([1,5,3,4,1,5,4])==3
