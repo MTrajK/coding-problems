94bc712c2f25904f33c6f1e07f4132662f8112de
RidaIftikhar14 <122225638+RidaIftikhar14@users.noreply.github.com>
2023-02-24 16:33:38 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from Math.odd_sum import odd_sum
 from Math.prime_factors import prime_factors
-assert odd_sum(1,3)==4
-assert prime_factors(50)==[2,5,5]
+
+def test_odd():
+  assert odd_sum(1,3)==4
+  
+def test_prime():
+  assert prime_factors(50)==[2,5,5]
 
 
